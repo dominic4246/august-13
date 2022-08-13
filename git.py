676ab854c44ae9df60ca66f48fba9ccb4e2cb5fe
Dominic Hahm <dominichahm@Dominics-MacBook-Pro.local>
2022-08-13 16:18:06 +0900

    most frequent letter in a string

--- git.py
+++ git.py
@@ -1,14 +1,11 @@
-# gift writing process #
+def mostLetter(string):
 
-# 1. initialize the set-up
-# 2. add all the gifts into one box
-# 3. write message that describes the content
-# 4. push the gift with address
-# 5. confirmation (1st time only)
-
-
-
-# github commmands for pushing #
+  most = string[0]
+  for i in range(0, len(string)):
+    if string.count(most) < string.count(string[i]):
+      most = string[i]
+  return most
+    
 
 
-print("hello")
+print(mostLetter("ccda"))
